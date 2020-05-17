69a4b2159879940676029069ce27962a27efe6a8
1653015 <46431109+1653015@users.noreply.github.com>
2020-05-17 15:28:02 +0700

    Updates

---
+++ precache-manifest.406239285879e5781a0791fc95dde7d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b298243964207106873777ba644d5be",
+    "url": "/BBook-Frontend/index.html"
+  },
+  {
+    "revision": "20edb2de62c79416e891",
+    "url": "/BBook-Frontend/static/css/main.a6cd1894.chunk.css"
+  },
+  {
+    "revision": "e9770c4b226c32902724",
+    "url": "/BBook-Frontend/static/js/2.0c908e73.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/BBook-Frontend/static/js/2.0c908e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20edb2de62c79416e891",
+    "url": "/BBook-Frontend/static/js/main.3ac9beee.chunk.js"
+  },
+  {
+    "revision": "5eb0e0c86a6c1788d28a",
+    "url": "/BBook-Frontend/static/js/runtime-main.4897ac02.js"
+  },
+  {
+    "revision": "6e3a5e0e8213f9dea5999b6bb99baebd",
+    "url": "/BBook-Frontend/static/media/background.6e3a5e0e.jpg"
+  }
+]);
