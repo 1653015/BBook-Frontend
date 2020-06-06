02f880ec9a3ccffb494e6d262f7d9df4b6322368
1653015 <46431109+1653015@users.noreply.github.com>
2020-06-06 16:43:35 +0700

    Updates

---
+++ precache-manifest.1eda4b52f74a4b92e0fc99f6e1f89aa2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8441cbb63f791e11d92a235f7108d9ce",
+    "url": "/BBook-Frontend/index.html"
+  },
+  {
+    "revision": "0c2c5d93c9fb511aa6aa",
+    "url": "/BBook-Frontend/static/css/main.9ba4d316.chunk.css"
+  },
+  {
+    "revision": "7392a8f957ca7396437a",
+    "url": "/BBook-Frontend/static/js/2.d5460fff.chunk.js"
+  },
+  {
+    "revision": "04a18400b8e590c01304dd636ca313b8",
+    "url": "/BBook-Frontend/static/js/2.d5460fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c2c5d93c9fb511aa6aa",
+    "url": "/BBook-Frontend/static/js/main.9cc2a94c.chunk.js"
+  },
+  {
+    "revision": "5eb0e0c86a6c1788d28a",
+    "url": "/BBook-Frontend/static/js/runtime-main.4897ac02.js"
+  },
+  {
+    "revision": "8d80fc1c9aa88b3d756b37b37b060b04",
+    "url": "/BBook-Frontend/static/media/booktitle.8d80fc1c.jpg"
+  },
+  {
+    "revision": "891cbcab6821a1113b15d2d8a060414d",
+    "url": "/BBook-Frontend/static/media/shoppingCart.891cbcab.png"
+  },
+  {
+    "revision": "96a666525a87827d3b761aac1451db9c",
+    "url": "/BBook-Frontend/static/media/wibubg.96a66652.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BBook-Frontend/precache-manifest.9d76d294c0337b1571a588aae1db19fe.js"
+  "/BBook-Frontend/precache-manifest.1eda4b52f74a4b92e0fc99f6e1f89aa2.js"
 );
 
 self.addEventListener('message', (event) => {
