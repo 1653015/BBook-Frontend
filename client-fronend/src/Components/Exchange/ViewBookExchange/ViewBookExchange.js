0180d908bd67ef8d0042248653f59c9055f3f71a
nacanh <1653009@student.hcmus.edu.vn>
2020-06-10 18:46:13 +0700

    Edit view-book-exchange

--- client-fronend/src/Components/Exchange/ViewBookExchange/ViewBookExchange.js
+++ client-fronend/src/Components/Exchange/ViewBookExchange/ViewBookExchange.js
@@ -3,16 +3,24 @@ import './ViewBookExchange.css';
 import { Redirect, Link} from 'react-router-dom';
 import Item from '../../Home/BookSlider/Item/Item';
 import Cookies from 'universal-cookie';
+import Carousel from "react-elastic-carousel";
+
+const breakPoints = [
+    { width: 1, itemsToShow: 5, itemsToScroll: 5},
+];
+
 class ViewBookExchange extends Component {
     constructor(props){
         super(props);
         this.state = {
             cookies: new Cookies(),
-            books: []
+            books: [],
+            tradedBooks: [],
         };
     }
+
     componentWillMount(){
-        fetch('https://cors-anywhere.herokuapp.com/https://bbook-backend.herokuapp.com/traderq/user', {
+        fetch('https://cors-anywhere.herokuapp.com/https://bbook-backend.herokuapp.com/user/books/stash', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -24,42 +32,100 @@ class ViewBookExchange extends Component {
             if (json.success) {
                 this.setState({books: json.books.books});
             }
-        })        
+        })
+        
+        fetch('https://cors-anywhere.herokuapp.com/https://bbook-backend.herokuapp.com/user/books/traded', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': this.props.cookies.get('u_t')
+            }
+        })
+        .then(res => res.json())
+        .then(json => {
+            if (json.success) {
+                this.setState({tradedBooks: json.books.tradedBooks});
+            }
+        })
     }
-  // componentDidMount(){}
-  // componentWillUnmount(){}
 
-  // componentWillReceiveProps(){}
-  // shouldComponentUpdate(){}
-  // componentWillUpdate(){}
-  // componentDidUpdate(){}
+    myArrow({ type, onClick}) {
+        const pointer = type === 'PREV' ? '❮' : '❯';
+        return (
+            <button onClick={onClick} className="button-slider">
+                {pointer}
+            </button>
+        );
+    }
+
+    // componentWillUnmount(){}
+
+    // componentWillReceiveProps(){}
+    // shouldComponentUpdate(){}
+    // componentWillUpdate(){}
+    // componentDidUpdate(){}
 
     render() {
-        if(!this.props.cookies.get('isLogin')){
+        if(!this.state.cookies.get('isLogin')){
             return(<Redirect path='/'/>)
         }
-        return (
-            <div className='container'>
-                <div className="ViewBookExchange">
+
+        if (this.state.books.length !== 0 || this.state.tradedBooks.length !== 0) {
+            return (
+                <div className="container">
+                    <div className="UserBookStorage">
                     <Link  className="post-trade-book"to="/exchange/create/book">Đăng ký đổi sách</Link>
-                    <div className="ViewBookExchange-content">
-                    {
-                        this.state.books.map(book => (
-                            <div key={book._id} className="item-box">
-                                <Item 
-                                    categorieID={book.categories[0]} 
-                                    key_data={book._id} 
-                                    image={book.image} 
-                                    name={book.name} 
-                                    author={book.author}/>
-                            </div>
-                        ))
-                    }
+                        <div className="BookSlider">
+                            <div className="book-slider-title">Sách đổi của bạn</div>
+                            {
+                                
+                                this.state.books.length === 0 ? (null) : (
+                                    <Carousel breakPoints={breakPoints} transitionMs={2000} disableArrowsOnEnd={false} renderArrow={this.myArrow}>
+                                        {
+                                            this.state.books.map(book => (
+                                                <Item 
+                                                    categorieID={this.props.data_key} 
+                                                    key={book._id} 
+                                                    key_data={book._id} 
+                                                    image={book.image} 
+                                                    name={book.name} 
+                                                    author={book.author} />
+                                            ))
+                                        }
+                                    </Carousel>
+                                )
+                            }
+                        </div>
+                        <div className="BookSlider">
+                            <div className="book-slider-title">Yêu cầu bạn đang gửi</div>
+                            {
+                                this.state.tradedBooks.length === 0 ? (null) : (
+                                    <Carousel breakPoints={breakPoints} transitionMs={2000} disableArrowsOnEnd={false} renderArrow={this.myArrow}>
+                                        {
+                                            this.state.tradedBooks.map(book => (
+                                                <Item 
+                                                    categorieID={this.props.data_key} 
+                                                    key={book._id} 
+                                                    key_data={book._id} 
+                                                    image={book.image} 
+                                                    name={book.name} 
+                                                    author={book.author} />
+                                            ))
+                                        }
+                                    </Carousel>
+                                )
+                            }
+                        </div>
                     </div>
                 </div>
-            </div>
-        );      
+            )
+        } else {
+            return (
+                <div className="prompt">
+                    <h1 className="message">Chưa có sách nào trao đổi</h1>
+                </div>
+            )
+        }
     }
 }
-
 export default ViewBookExchange;
